c27c3aaa5fd23da62530bbf02bfe92c927fbb276
mihirKathiria <89318023+mihirKathiria@users.noreply.github.com>
2022-11-13 18:15:31 +0530

    Initial commit

---
+++ assets/js/register.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    var getData;
+    $("#signin_here").click(function (e) {
+        var password = $("#signin_pass").val()
+        var repass = $("#signin_repass").val()
+        if (password.match(repass)) {
+            e.preventDefault();
+            $.ajax({
+                type: "POST",
+                url: "php/registration_success.php",
+                data: {
+                    fullname: $("#signin_fullname").val(),
+                    monumber: $("#signin_mobile_no").val(),
+                    email: $("#signin_email").val(),
+                    password: $("#signin_pass").val()
+                },
+                beforeSend: function () {
+                    $("#signin_here").html("Please Wait...");
+                },
+                success: function (response) {
+                    if (response.trim() == "Sign in Success :)") {
+                        window.location = "login.html";
+                    }
+                }
+            });
+        } else {
+            alert("Please Confirm Pasword Or Recheck")
+        }
+    });
+});
